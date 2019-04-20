c6aff7ef4097c33635da27029512677f915c38f4
mingyoung <mingyoungcheung@gmail.com>
2019-04-20 11:48:10 +0800

    build

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:EasyDingTalk/easydingtalk.github.io.git master
+git push -f https://${GITHUB_TOKEN}@github.com/EasyDingTalk/easydingtalk.github.io.git master
 
 cd -
