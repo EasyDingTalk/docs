560b4429cfe70e22e937fa90caa0be5f3f6c7a40
mingyoung <mingyoungcheung@gmail.com>
2020-04-25 17:53:39 +0800

    add ga

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,15 @@ module.exports = {
     title: 'EasyDingTalk',
     description: 'EasyDingTalk 是一个易于使用，基于 MIT 协议开发的高质量开源钉钉第三方 PHP SDK。',
 
+    plugins: [
+        [
+            '@vuepress/google-analytics',
+            {
+                'ga': 'UA-132433251-1'
+            }
+        ]
+    ],
+
     themeConfig: {
         nav: [
             { text: 'GitHub', link: 'https://github.com/mingyoung/dingtalk' }
---
+++ docs/.vuepress/dist/assets/js/5.1f6a2a99.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{326:function(t,e,n){"use strict";n.r(e);var s=n(33),r=Object(s.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[e("meta",{attrs:{"http-equiv":"refresh",content:"0;url=/start.html"}})])}),[],!1,null,null,null);e.default=r.exports}}]);
